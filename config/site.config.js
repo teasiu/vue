5e96101f0914609f4478b00a2742e5ad36a77daf
teasiu <teasiu@qq.com>
2022-11-06 04:39:49 +0800

    update

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/teasiu/vue" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by teasiu.',
+    '本站由官网 <a href="https://ecoo.top" target="_blank" rel="noopener noreferrer">ecoo.top</a>.  ❤ 倾心制作.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
@@ -54,6 +54,10 @@ module.exports = {
     {
       name: 'GitHub',
       link: 'https://github.com/teasiu/vue',
+    },
+    {
+      name: '官方网站',
+      link: 'https://www.ecoo.top',
     },
     {
       name: 'Telegram',
