d7ccc0cbbc1522d39212a8a190723d5e2655c76c
teasiu <teasiu@qq.com>
2023-01-07 11:00:23 +0800

    update new onedrive.ecoo.top

--- config/api.config.js
+++ config/api.config.js
@@ -11,7 +11,7 @@ module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
   clientId: '8ed71c2d-0ce8-434f-aba2-23fdc7508668',
-  obfuscatedClientSecret: 'U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==',
+  obfuscatedClientSecret: 'U2FsdGVkX19O6l8MtGpAKOoHKilax2IxUxsWoc4ArTqSIKdYF6TZkpM7+pKZCjAJuj2oisPNf4cqmaQOUf1DSw==',
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
